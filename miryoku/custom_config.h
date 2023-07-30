bd2a041071d039e791a24efd4ad6b65b55ce3377
Dmitriy Krasilnikov <krasilnikov.d.o@gmail.com>
2023-07-30 20:17:51 +0200

    swap tab and space

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,7 +6,7 @@
 &kp Q,             &kp W,             U_MT(RALT, F),     &kp P,             &kp B,             &kp J,             &kp L,             U_MT(RALT, U),     &kp Y,             &kp SQT,           \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, TAB),U_LT(U_MOUSE, SPACE),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_LAYER_MOUSE \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &kp B,             U_NA,              U_RDO,             &kp J,             U_CPY,             U_CUT,             U_UND,             \
